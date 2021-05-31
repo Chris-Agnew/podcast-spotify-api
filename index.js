69e661854cb5544f5f5d329aca40420e558146b8
Chris <chris@chris-agnew.com>
2021-05-31 15:13:39 -0400

    ADD: JS form validity for errors and success

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("im linked correctly");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
+const form = document.getElementById("form");
+
+const validate = () => {
+  form.addEventListener("submit", (e) => {
+    let messages = [];
+    if (email.value == "" || email.value == null) {
+      messages.push("Oops! That doesn't look like an email address");
+    }
+
+    if (messages.length > 0) {
+      e.preventDefault();
+      error.innerText = messages.join(", ");
+    }
+  });
+};
+
+validate();
+
+const submit = () => {
+  form.addEventListener("submit", () => {
+    alert("Access Requested, we will get in touch soon");
+  });
+};
+
+submit();
